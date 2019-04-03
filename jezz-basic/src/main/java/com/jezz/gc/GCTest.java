f8ab92b5222836adebfd644d49c20a89b35a8d4d
jezz <243284589@qq.com>
2019-04-03 18:32:22 +0800

    java 虚拟机gc

--- jezz-basic/src/main/java/com/jezz/gc/GCTest.java
+++ jezz-basic/src/main/java/com/jezz/gc/GCTest.java
@@ -10,11 +10,7 @@ public class GCTest {
 
     private byte[] bigSize = new byte[2 * _1MB];
 
-    /**
-     * -verbose:gc
-     */
-    @Test
-    public void test1(){
+    private void doGC(){
         GCTest objectA = new GCTest();
         GCTest objectB = new GCTest();
         objectA.instance = objectB;
@@ -22,5 +18,43 @@ public class GCTest {
         objectA = null;
         objectB = null;
         System.gc();
+    }
+
+    /**
+     * -verbose:gc
+     */
+    @Test
+    public void test1(){
+        doGC();
+    }
+
+
+    // -XX:+PrintGCDetails -XX:+UseSerialGC
+    @Test
+    public void serial_serialOld(){
+        doGC();
+    }
+
+    // -XX:+PrintGCDetails -XX:+UseParNewGC
+    @Test
+    public void parNew_serialOld(){
+        doGC();
+    }
+
+    // -XX:+PrintGCDetails -XX:+UseParallelGC
+    @Test
+    public void parallel_serialOld(){
+        doGC();
+    }
+
+    // -XX:+PrintGCDetails -XX:+UseConcMarkSweepGC
+    @Test
+    public void parNew_Cms_SerialOld(){
+        doGC();
+    }
+
+    @Test
+    public void parallel_parallelOld(){
+        doGC();
     }
 }
