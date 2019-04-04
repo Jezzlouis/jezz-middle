df0c16b44d68f1e4a3a6657bd1de308584c68f76
jezz <243284589@qq.com>
2019-04-04 09:42:26 +0800

    java 虚拟机gc

---
+++ jezz-basic/src/main/java/com/jezz/gc/MetaspaceOOMTest.java
@@ -0,0 +1,41 @@
+package com.jezz.gc;
+
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class MetaspaceOOMTest {
+
+    /**
+     * JVM参数:-XX:MetaspaceSize=8m -XX:MaxMetaspaceSize=128m -XX:+PrintFlagsInitial
+     */
+    public static void main(String[] args) {
+        int i = 0;
+
+        try {
+            for (;;) {
+                i++;
+
+                Enhancer enhancer = new Enhancer();
+                enhancer.setSuperclass(OOMObject.class);
+                enhancer.setUseCache(false);
+                enhancer.setCallback(new MethodInterceptor() {
+                    public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+                        return proxy.invokeSuper(obj, args);
+                    }
+                });
+                enhancer.create();
+            }
+        } catch (Exception e) {
+            System.out.println("第" + i + "次时发生异常");
+            e.printStackTrace();
+        }
+    }
+
+    static class OOMObject {
+
+    }
+
+}
