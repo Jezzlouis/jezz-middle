212429a65a78b987351e1efb6182109a1fc289c9
jezz <243284589@qq.com>
2019-04-09 16:41:59 +0800

    springboot1.x redis 成功 springboot2.x redis 踩坑失败

---
+++ jezz-redis/src/main/java/com/jezz/config/JedisRedisConfig.java
@@ -0,0 +1,112 @@
+package com.jezz.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisNode;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Configuration
+public class JedisRedisConfig {
+
+
+    @Value("${spring.redis.clusterNodes}")
+    private String clusterNodes;
+    @Value("${spring.redis.maxActive}")
+    private int maxActive;
+    @Value("${spring.redis.maxIdle}")
+    private int maxIdle;
+    @Value("${spring.redis.maxWait}")
+    private int maxWait;
+    @Value("${spring.redis.minIdle}")
+    private int minIdle;
+    @Value("${spring.redis.commandTimeout}")
+    private int commandTimeout;
+    @Value("${spring.redis.maxAttempts}")
+    private int maxAttempts;
+    @Value("${spring.redis.password}")
+    private String password;
+
+
+    /**
+     * springboot1.x使用的jedis进行连接 Jedis在实现上是直接连接的redis server，
+     * 如果在多线程环境下是非线程安全的，这个时候只有使用连接池，为每个Jedis实例增加物理连接
+     */
+    @Bean
+    public RedisConnectionFactory jedisConnectionFactory() {
+        String[] hostAndPorts = clusterNodes.split(",");
+        Set<RedisNode> redisNodeSet = Arrays.stream(hostAndPorts).map(host -> new RedisNode(host.split(":")[0].trim(), Integer.valueOf(host.split(":")[1].trim()))).collect(Collectors.toSet());
+        RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration();
+        clusterConfiguration.setClusterNodes(redisNodeSet);
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxWaitMillis(maxWait);
+        jedisPoolConfig.setMaxIdle(maxIdle);
+        jedisPoolConfig.setMinIdle(minIdle);
+        jedisPoolConfig.setMaxTotal(maxActive);
+        JedisConnectionFactory factory = new JedisConnectionFactory(clusterConfiguration,jedisPoolConfig);
+        factory.setPassword(password);
+        return factory;
+    }
+
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate redisTemplate = new RedisTemplate();
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        JdkSerializationRedisSerializer jdkSerializationRedisSerializer = new JdkSerializationRedisSerializer();
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setValueSerializer(jdkSerializationRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashValueSerializer(jdkSerializationRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        stringRedisTemplate.setConnectionFactory(redisConnectionFactory);
+        stringRedisTemplate.afterPropertiesSet();
+        return stringRedisTemplate;
+    }
+
+    @Bean
+    public JedisCluster jedisCluster() {
+        String[] serverArray = clusterNodes.split(",");
+        Set<HostAndPort> nodes = Arrays.stream(serverArray).map(host -> new HostAndPort(host.split(":")[0].trim(), Integer.valueOf(host.split(":")[1].trim()))).collect(Collectors.toSet());
+        GenericObjectPoolConfig config = new GenericObjectPoolConfig();
+        config.setMinIdle(minIdle);
+        config.setMaxIdle(maxIdle);
+        config.setMaxTotal(maxActive);
+        return new JedisCluster(nodes, commandTimeout, commandTimeout, maxAttempts, password, config);
+    }
+}
