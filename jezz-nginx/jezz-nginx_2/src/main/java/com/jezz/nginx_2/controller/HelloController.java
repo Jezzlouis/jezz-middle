008472b6f33c91725e2515b3cdaeb338293e6b92
jezz <243284589@qq.com>
2019-07-23 11:57:54 +0800

    nginx 测试

---
+++ jezz-nginx/jezz-nginx_2/src/main/java/com/jezz/nginx_2/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.jezz.nginx_2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "8082";
+    }
+}
