49582eebbabe478b2859973775b1b1310e7a8e8b
jezz <243284589@qq.com>
2019-08-12 10:10:37 +0800

    dubboFilter

--- jezz-dubbo/dubbo-consumer/src/main/java/com/jezz/dubbo/consumer/filter/TraceFilter.java
+++ jezz-dubbo/dubbo-consumer/src/main/java/com/jezz/dubbo/consumer/filter/TraceFilter.java
@@ -2,11 +2,14 @@ package com.jezz.dubbo.consumer.filter;
 
 import com.alibaba.dubbo.common.Constants;
 import com.alibaba.dubbo.common.extension.Activate;
+import com.alibaba.dubbo.common.utils.StringUtils;
 import com.alibaba.dubbo.rpc.*;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
+
+import java.util.Date;
 
 @Slf4j
 @Activate(group = {Constants.CONSUMER},order = 1)
@@ -17,9 +20,12 @@ public class TraceFilter implements Filter {
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         RpcContext context = RpcContext.getContext();
-        String chTraceId = MDC.get("traceId");
-        LOGGER.debug("chTraceId={}", chTraceId);
-        context.setAttachment("chTraceId", chTraceId);
+        String chTraceId = MDC.get("trace_id");
+        if(StringUtils.isEmpty(chTraceId)){
+            chTraceId = new Date().getTime() + "-" + ((int) ((Math.random() * 9 + 1) * 100000));
+        }
+        LOGGER.warn("trace_id={}", chTraceId);
+        context.setAttachment("trace_id", chTraceId);
         return invoker.invoke(invocation);
     }
 }
--- jezz-dubbo/dubbo-provider/src/main/java/com/jezz/dubbo/provider/filter/TraceFilter.java
+++ jezz-dubbo/dubbo-provider/src/main/java/com/jezz/dubbo/provider/filter/TraceFilter.java
@@ -13,10 +13,10 @@ public class TraceFilter implements Filter {
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         RpcContext context = RpcContext.getContext();
-        String chTraceId = context.getAttachment("chTraceId") + "&client=" + context.getRemoteAddressString();
-        log.debug("chTraceId={}", chTraceId);
+        String chTraceId = context.getAttachment("trace_id") + "&client=" + context.getRemoteAddressString();
+        log.warn("trace_id={}", chTraceId);
         if (StringUtils.isNotEmpty(chTraceId)) {
-            MDC.put("traceId", chTraceId);
+            MDC.put("trace_id", chTraceId);
         }
         Result result = invoker.invoke(invocation);
         if (StringUtils.isNotEmpty(chTraceId)) {
