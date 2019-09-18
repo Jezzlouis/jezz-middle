84a426b26a5014f7ad25eb3a41b727192c45ce7a
jezz <243284589@qq.com>
2019-09-18 15:17:30 +0800

    分布式

--- jezz-leetcode/src/main/java/com/jezz/leetcode/Reverse.java
+++ jezz-leetcode/src/main/java/com/jezz/leetcode/Reverse.java
@@ -8,31 +8,7 @@ import java.util.List;
 
 public class Reverse {
     public static int reverse(int x) {
-        String a = String.valueOf(x);
-        boolean flag = false;
-        List<String> r = new ArrayList<>();
-        for (int i = 0; i < a.length(); i++) {
-            if("-".equals(String.valueOf(a.charAt(i)))){
-                 flag = true;
-                 continue;
-            }
-            r.add(String.valueOf(a.charAt(i)));
-        }
-        StringBuilder b = new StringBuilder();
-        if (flag){
-            b.append("-");
-        }
-        for (int i = r.size()-1; i >= 0; i--) {
-            if ("0".equals(r.get(i))) {
-                break;
-            }
-            b.append(r.get(i));
-        }
-        int result = Integer.valueOf(b.toString());
-        if(result > (1L << 32) || result < (-1L << 32)){
-            return 0;
-        }
-        return result;
+        return 0;
     }
 
     public static void main(String[] args) {
